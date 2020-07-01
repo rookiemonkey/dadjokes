d60823d5dfb52d30075575f51a3fea789b0bc526
Kevin Roi Basina <kevinroirigorbasina@pm.me>
2020-07-01 19:15:08 +0800

    10  jokes loaded from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            jokes: []
+        }
+    }
+
+    async componentDidMount() {
+        fetch('https://icanhazdadjoke.com/search?limit=10', {
+            headers: { 'Accept': 'application/json' }
+        })
+            .then(r => { return r.json() })
+            .then(j => { this.setState(st => { st.jokes = [...j.results] })})
+            .catch(e => { return new Error(`Error upon fetching 10 jokes ${e}`) })
+    }
+
     render() {
         return null
     }
