48e1c7e932293e55893db8a5f41977cdd5bc3331
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-22 20:25:44 +0800

    added a howler and a background music

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactHowler from 'react-howler'
 import './assets/scss/_index.scss';
 import './assets/vendor/bootstrap-grid.min.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ReactHowler
+      src={require('./assets/sounds/sillychicken.mp3')}
+      playing={true}
+      loop={true}
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
